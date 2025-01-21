b0b81f5f46a3dc1d8b4fd5c44d95bda2d5cc64e5
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-01-21 19:20:27 +0100

    added rr and rrr to lower moves but it didn't reduce very much...

--- algo.c
+++ algo.c
@@ -6,7 +6,7 @@
 /*   By: lefoffan <lefoffan@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 14:22:23 by lefoffan          #+#    #+#             */
-/*   Updated: 2025/01/21 14:24:44 by lefoffan         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:16:20 by lefoffan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,20 +75,27 @@ t_list	*get_cheap(t_list *a)
 void	ft_sort_jsp2(t_list **a, t_list **b)
 {
 	t_list	*to_push;
+	int		dist_a;
+	int		dist_b;
 
 	while (*a)
 	{
 		to_push = get_cheap(*a);
-		move(ft_lstsize(*a), get_dist(*a, to_push), a, "a");
+		// move(ft_lstsize(*a), get_dist(*a, to_push), a, "a");
+		dist_a = get_dist(*a, to_push);
 		if (ft_lstsize(*b) >= 2)
 		{
 			if (to_push->content < ft_get_min(*b)->content)
-				move(ft_lstsize(*b), get_dist(*b, ft_get_min(*b)) + 1, b, "b");
+				dist_b = get_dist(*b, ft_get_min(*b)) + 1;
+				// move(ft_lstsize(*b), get_dist(*b, ft_get_min(*b)) + 1, b, "b");
 			else if (to_push->content > ft_get_max(*b)->content)
-				move(ft_lstsize(*b), get_dist(*b, ft_get_max(*b)), b, "b");
+				dist_b = get_dist(*b, ft_get_max(*b));
+				// move(ft_lstsize(*b), get_dist(*b, ft_get_max(*b)), b, "b");
 			else
-				move(ft_lstsize(*b), get_dist(*b, get_smaller(*b, to_push)), b, "b");
+				dist_b = get_dist(*b, get_smaller(*b, to_push));
+				// move(ft_lstsize(*b), get_dist(*b, get_smaller(*b, to_push)), b, "b");
 		}
+		move_both(dist_a, dist_b, a, b);
 		ft_push(a, b, "pb\n");
 	}
 	while (ft_lstlast(*b) != ft_get_min(*b))
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: lefoffan <lefoffan@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 09:50:12 by lefoffan          #+#    #+#             */
-/*   Updated: 2025/01/21 11:46:08 by lefoffan         ###   ########.fr       */
+/*   Updated: 2025/01/21 18:50:12 by lefoffan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ void	ft_rotate(t_list **list, char *move);
 void	ft_rev_rotate(t_list **list, char *move);
 void	ft_push(t_list **a, t_list **b, char *move);
 void	ft_sort(t_list **a, t_list **b);
-void	ft_move_to_top(t_list **a, t_list *target);
 // void	ft_set_index(t_list **a);
 t_list	*get_smaller(t_list *lst, t_list *node);
 t_list	*ft_get_min(t_list *a);
 t_list	*ft_get_max(t_list *a);
-void	ft_place_between(t_list *to_push, t_list **b);
+// void	ft_place_between(t_list *to_push, t_list **b);
 t_list	*ft_lst_init(char **av);
 void	print(t_list *a, t_list *b);
 void	move(int size, int dist, t_list **lst, char *name_list);
+void	move_both(int dist_a, int dist_b, t_list **a, t_list **b);
 
 #endif
